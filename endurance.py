4be19de1af077013be2abdeefa31ef8222a5a71b
Vittorio <vitty157@gmail.com>
2019-04-06 12:28:49 +0000

    endurance

--- endurance.py
+++ endurance.py
@@ -1,22 +1,25 @@
 print("inseirisci carburante e consumo orario per calcolare tempo di volo")
 
+
+	
 g=input("galloni:")
 g=int(g)
 
 c=input("consumo\h:")
 c=int(c)
-r=g/c
+
+if g<0 or c<0:
+	print("errore")
+else:
+  r=g/c
 
 
-h=int(r) #ore
+  h=int(r) #ore
 
-r=(r-h)*60
-m=int(r)  #minuti
+  r=(r-h)*60
+  m=int(r)  #minuti
 
-s=(r-m)*60
-s=int(s) #secondi
+  s=(r-m)*60
+  s=int(s) #secondi
 
-
-
-
-print("il consumo è",h,"h",m,"m",s,"s")
+  print("il tempo è",h,"h",m,"m",s,"s")
